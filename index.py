efb0e7df41693e91f9357b75136a700d18b25ae8
juanjose23 <jrios9836@gmail.com>
2024-03-30 17:11:04 -0600

    cambios

--- index.py
+++ index.py
@@ -4,12 +4,13 @@ import requests
 from datetime import datetime
 import uuid
 import os
+import subprocess
 #Conexion con nuestro BOT
 TOKEN = '7183470855:AAGgo_eXb2POEsx6RjIRzCQI6bkwmvUOSFU'
 bot = telebot.TeleBot(TOKEN)
 PHOTOS_FOLDER = r"C:\Users\jrios\OneDrive\Escritorio\Detection-and-count-CBB\Broca2000\examp"
-
-
+ruta_bat = r'C:\Users\jrios\OneDrive\Escritorio\Bot_telegram\run_app.bat'
+TEXT_FOLDER = r'C:\Users\jrios\OneDrive\Escritorio\Detection-and-count-CBB\yolov5\runs\detect'
 
 # Manejar el comando /start
 @bot.message_handler(commands=['start'])
@@ -43,21 +44,6 @@ def send_help(message):
 def send_image_help(message):
     bot.reply_to(message, 'Por favor, envía una foto.')
 
-import subprocess
-
-# Ruta al archivo batch
-
-ruta_bat = r'C:\Users\jrios\OneDrive\Escritorio\Bot_telegram\run_app.bat'
-TEXT_FOLDER = r'C:\Users\jrios\OneDrive\Escritorio\Detection-and-count-CBB\yolov5\runs\detect'
-
-def get_latest_folder(directory):
-    folders = [f for f in os.listdir(directory) if os.path.isdir(os.path.join(directory, f))]
-    if folders:
-        folder=max(folders, key=os.path.getmtime)
-        print(folder)
-        return folder
-    else:
-        return None
 @bot.message_handler(content_types=['photo'])
 def save_photo(message):
     try:
@@ -65,26 +51,16 @@ def save_photo(message):
         file_info = bot.get_file(message.photo[-1].file_id)
         # Descargar la imagen
         downloaded_file = bot.download_file(file_info.file_path)
-        file_name = f"{message.photo[-1].file_id}.txt"
         # Guardar la imagen en la carpeta especificada
         file_path = os.path.join(PHOTOS_FOLDER, f"{message.photo[-1].file_id}.jpg")
         with open(file_path, 'wb') as new_file:
             new_file.write(downloaded_file)
 
         subprocess.run(ruta_bat, shell=True)
-         # Obtener la última carpeta creada en el directorio de detección
-        latest_folder = get_latest_folder(TEXT_FOLDER)
-        if latest_folder:
-            txt_file_path = os.path.join(TEXT_FOLDER, latest_folder, "labels",file_name)
-            with open(txt_file_path, 'r') as txt_file:
-                print(txt_file_path)
-                txt_content = txt_file.read()
 
-            # Enviar el contenido del archivo de texto como mensaje
-            bot.reply_to(message, "Aquí está el archivo de texto resultante:")
-            bot.send_message(message.chat.id, txt_content)
-        else:
-            bot.reply_to(message, "No se encontró ningún archivo de texto en la carpeta de detección.")
+         # Enviar el contenido del archivo de texto como mensaje
+        bot.reply_to(message, "Se ha recibido con éxito:")
+     
 
     except Exception as e:
         print(e)
